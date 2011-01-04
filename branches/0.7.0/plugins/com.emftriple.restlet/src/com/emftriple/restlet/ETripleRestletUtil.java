76aadd8dcaa4ba28f78af86b3783eac4a2770e86
g.hillairet <g.hillairet@189c7fc5-1132-1748-28da-600cb6a5e377>
2011-01-04 00:38:35 +0000

    Share project "com.emftriple.restlet" into "https://svn.codespot.com/a/eclipselabs.org/emftriple"

---
+++ branches/0.7.0/plugins/com.emftriple.restlet/src/com/emftriple/restlet/ETripleRestletUtil.java
@@ -0,0 +1,64 @@
+package com.emftriple.restlet;
+
+import javax.persistence.EntityManager;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.restlet.data.MediaType;
+import org.restlet.ext.rdf.RdfRepresentation;
+
+import com.emf4sw.rdf.RDFFactory;
+import com.emf4sw.rdf.RDFGraph;
+import com.emf4sw.rdf.resource.RDFResourceImpl.DummyRDFResource;
+import com.emftriple.impl.EObjectEntityManager;
+import com.emftriple.transform.PutObject;
+import com.emftriple.transform.impl.PutObjectImpl;
+
+/**
+ * 
+ * @author ghillairet
+ * @since 0.1.0
+ */
+public class ETripleRestletUtil {
+	
+	public static RdfRepresentation getRepresentation(Resource resource, EntityManager em, MediaType mediaType) {
+		final PutObject put = new PutObjectImpl(((EObjectEntityManager)em).getMapping(), 
+				((EObjectEntityManager)em).getDelegate());
+		
+		final RDFGraph aGraph = RDFFactory.eINSTANCE.createDocumentGraph();
+		final Resource res = new DummyRDFResource();
+		res.getContents().add(aGraph);
+		
+		for (EObject obj: resource.getContents())
+			put.put(obj, aGraph);
+		
+		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(aGraph), mediaType);
+	}
+	
+	public static RdfRepresentation getRepresentation(Iterable<? extends EObject> objects, EntityManager em, MediaType mediaType) {
+		final PutObject put = new PutObjectImpl(((EObjectEntityManager)em).getMapping(), 
+				((EObjectEntityManager)em).getDelegate());
+		
+		final RDFGraph aGraph = RDFFactory.eINSTANCE.createDocumentGraph();
+		final Resource res = new DummyRDFResource();
+		res.getContents().add(aGraph);
+		
+		for (EObject o: objects)
+			put.put(o, aGraph);
+		
+		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(aGraph), mediaType);
+	}
+	
+	public static RdfRepresentation getRepresentation(EObject object, EntityManager em, MediaType mediaType) {
+		final PutObject put = new PutObjectImpl(((EObjectEntityManager)em).getMapping(), 
+				((EObjectEntityManager)em).getDelegate());
+		
+		final RDFGraph aGraph = RDFFactory.eINSTANCE.createDocumentGraph();
+		final Resource res = new DummyRDFResource();
+		res.getContents().add(aGraph);
+		
+		put.put(object, aGraph);
+		
+		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(aGraph), mediaType);
+	}
+}
