cdfa1537daebd48ae02a8cc84b65d02733c22cad
g.hillairet <g.hillairet@189c7fc5-1132-1748-28da-600cb6a5e377>
2011-01-04 16:16:00 +0000

    [CHANGE] Automatically Generated Sparql Queries are now cut in several sub queries in order to allow execution of large queries over sparql services, such as dbpedia

--- branches/0.7.0/plugins/com.emftriple/src/com/emftriple/transform/impl/AbstractGetObject.java
+++ branches/0.7.0/plugins/com.emftriple/src/com/emftriple/transform/impl/AbstractGetObject.java
@@ -7,14 +7,25 @@
  */
 package com.emftriple.transform.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
+import com.emf4sw.rdf.RDFFactory;
 import com.emf4sw.rdf.RDFGraph;
 import com.emf4sw.rdf.Resource;
+import com.emf4sw.rdf.resource.RDFResource;
+import com.emf4sw.rdf.resource.RDFResourceImpl.DummyRDFResource;
 import com.emftriple.Mapping;
 import com.emftriple.datasources.EntityDataSourceManager;
 import com.emftriple.resource.ETripleResource.ResourceManager;
@@ -28,52 +39,91 @@ import com.emftriple.util.SparqlQueries;
  * @since 0.6.0
  */
 public abstract class AbstractGetObject implements GetObject {
-	
+
 	protected Mapping mapping;
 
 	protected EntityDataSourceManager dataSourceManager; 
-	
+
 	protected ResourceManager manager;
-	
+
 	protected AbstractGetObject(ResourceManager manager, Mapping mapping, EntityDataSourceManager dataSourceManager) {
 		this.manager = manager;
 		this.mapping = mapping;
 		this.dataSourceManager = dataSourceManager;
 	}
 
+//	protected RDFGraph getGraph(EClass eClass, URI key) {
+//		final List<String> queries = SparqlQueries.constructSubjectService(key, eClass);
+//		final List<RDFGraph> graphs = new ArrayList<RDFGraph>();
+//		
+//		for (final String str: queries) {
+//			RDFGraph aGraph = 
+//				dataSourceManager.executeConctructQuery(str);
+//			if (aGraph != null)
+//				graphs.add(aGraph);
+//		}
+//		
+//		return graphs.isEmpty() ? null : (graphs.size() == 1 ? graphs.get(0) : merge(graphs));
+//	}
+	
 	protected RDFGraph getGraph(EClass eClass, URI key) {
-		final String aQuery;
-//		final URI graphURI = EntityUtil.getNamedGraph(eClass);
-		
-//		if (graphURI == null) {
-			aQuery = SparqlQueries.constructSubject(key, eClass);
-//		} else {
-//			aQuery = SparqlQueries.describe(key, graphURI);
-//		}
-		
-		return dataSourceManager.executeDescribeQuery(aQuery);
+		final List<String> queries = SparqlQueries.constructSubjectService(key, eClass);
+		final List<RDFGraph> graphs = new ArrayList<RDFGraph>();
+		final ExecutorService executor = Executors.newFixedThreadPool(4);
+		final List<Future<RDFGraph>> futures = new ArrayList<Future<RDFGraph>>();
+
+		for (final String str: queries) {
+			Callable<RDFGraph> call = new Callable<RDFGraph>() {
+				@Override
+				public RDFGraph call() throws Exception {
+					System.out.println("call " + str);
+					return dataSourceManager.executeConctructQuery(str);
+				}};
+				futures.add( executor.submit(call) );
+		}
+
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+		}
+
+		for (Future<RDFGraph> f: futures) {
+			RDFGraph g;
+			try {
+				g = f.get();
+				if (g != null)
+					graphs.add(g);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return graphs.isEmpty() ? null : (graphs.size() == 1 ? graphs.get(0) : merge(graphs));
 	}
-	
+
 	protected <T> RDFGraph getGraph(Class<T> entityClass, URI key) {
-		final String aQuery;
-//		final URI graphURI = mapping.getNamedGraph(entityClass);
 		final EClass eClass = mapping.getEClass(entityClass);
-		
-//		if (graphURI == null) {
-			aQuery = SparqlQueries.constructSubject(key, eClass);
-//		} else {
-//			aQuery = SparqlQueries.constructSubject(key, eClass);
-//		}
-		
-		return dataSourceManager.executeConctructQuery(aQuery);
+
+		return getGraph(eClass, key);
 	}
-	
+
+	private static RDFGraph merge(List<RDFGraph> graphs) {
+		RDFGraph aGraph = RDFFactory.eINSTANCE.createDocumentGraph();
+		RDFResource res = new DummyRDFResource();
+		res.getContents().add(aGraph);
+		for (RDFGraph g: graphs)
+			aGraph.add(g);
+
+		return aGraph;
+	}
+
 	protected void setIdValue(EObject returnedObject, Resource from, EAttribute id) {
 		if (id == null)
 			return;
-		
+
 		EAnnotation ann = EntityUtil.getETripleAnnotation(id, "Id");
-		
+
 		if (ann == null) {
 			ann = EntityUtil.getETripleAnnotation(id, "GeneratedId");
 			if (ann == null) {
@@ -81,7 +131,7 @@ public abstract class AbstractGetObject implements GetObject {
 				return;
 			}
 		}
-		
+
 		if (ann.getDetails().containsKey("base")) {
 			String base = ann.getDetails().get("base");
 			if (from.getURI().startsWith(base)) {
@@ -91,5 +141,5 @@ public abstract class AbstractGetObject implements GetObject {
 			}
 		}
 	}
-	
+
 }
--- branches/0.7.0/plugins/com.emftriple/src/com/emftriple/util/SparqlQueries.java
+++ branches/0.7.0/plugins/com.emftriple/src/com/emftriple/util/SparqlQueries.java
@@ -2,10 +2,13 @@ package com.emftriple.util;
 
 import static com.emftriple.util.EntityUtil.getRdfTypes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 import com.emf4sw.rdf.Node;
@@ -23,21 +26,21 @@ import com.google.common.collect.Lists;
  * @since 0.7.0
  */
 public class SparqlQueries {
-	
+
 	public static String ask(URI from, EClass eClass) {
 		final StringBuffer buffer = new StringBuffer("ASK { " + from.toString());
 		for (URI aURI: getRdfTypes(eClass)) {
 			buffer.append( " <" + RDF.type + "> <" + aURI.toString() + "> ");
 		}
 		buffer.append(" }");
-		
+
 		return buffer.toString();
 	}
 
 	public static String describe(URI from, URI graph) {
 		return "DESCRIBE <" + from.toString()+ ">";
 	}
-	
+
 	public static List<String> selectAllTypes(DataSourceManager manager, URIElement from) {
 		final List<String> types = Lists.newArrayList();
 		if (from != null && from.getURI() != null) 
@@ -63,67 +66,97 @@ public class SparqlQueries {
 	public static Integer countObjectsByType(DataSource source, EClass from) {
 		String query = "SELECT ?n WHERE { ";
 		List<URI> uris = getRdfTypes(from);
-		
+
 		query += "{ ?n <" + RDF.type + "> <" + uris.get(0) + "> } ";
 		for (int i=1;i<uris.size();i++) {
 			query += (i % 2 == 0) ? "" : "UNION " + "{ ?n <" + RDF.type + "> <" + uris.get(i) + "> } ";
 		}
 		query += "}";
-		
+
 		ResultSet rs = source.selectQuery(query);
 		int i=0;
 		for (;rs.hasNext();)
 			i += rs.next() != null ? 1 : 0;
-		
+
 		return i;
 	}
-	
+
 	public static String graphQuery(URI graph) {
 		return "CONSTRUCT { ?s ?p ?o } WHERE { GRAPH <" + graph.toString()+ "> { ?s ?p ?o} }";
 	}
-	
+
 	public static String typeOf(URI resource, URI uri) {
 		return "ASK WHERE { <" + 
-			resource.toString() + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <" + uri.toString() + "> }";
+		resource.toString() + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <" + uri.toString() + "> }";
 	}
-	
+
 	public static String typeOf(URI resource) {
 		return "SELECT ?type WHERE { <" + 
-				resource.toString() + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type }";
+		resource.toString() + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type }";
 	}
-	
+
 	public static String typeOf(String resourceURI) {
 		return "SELECT ?type WHERE { <" + 
-				resourceURI + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type }";
+		resourceURI + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type }";
 	}
 
 	public static String constructSubject(URI key, Object object) {
 		return "CONSTRUCT { <" + key + "> ?p ?o } WHERE { <" + key + "> ?p ?o }";
 	}
-	
+
 	public static String constructSubject(URI key, EClass eClass) {
+		return constructSubject(key, eClass, eClass.getEAllStructuralFeatures()).get(0);
+	}
+
+	public static List<String> constructSubjectService(URI key, EClass eClass) {
+		final List<String> queries = new ArrayList<String>();
+		final List<EAttribute> attrs = eClass.getEAllAttributes();
+		final List<EReference> refs = eClass.getEAllReferences();
+
+		if (!attrs.isEmpty()) {
+			// cuts the set of attributes to form small queries
+			// that can be executed on services
+			int MAX = 3;
+			int POS = 0;
+
+			while(POS < attrs.size()) {
+				List<EStructuralFeature> feats = new ArrayList<EStructuralFeature>();  
+				for (int i=POS; i < MAX; i++)
+					feats.add(attrs.get(i));
+				queries.addAll(constructSubject(key, eClass, feats));
+				POS+=MAX;
+				MAX+=MAX; if (MAX >= attrs.size()) MAX = attrs.size();
+			}
+		}
+		if (!refs.isEmpty())
+			queries.addAll(constructSubject(key, eClass, refs));
+
+		return queries;
+	}
+
+	private static List<String> constructSubject(URI key, EClass eClass, List<? extends EStructuralFeature> feats) {
+		final List<String> queries = new ArrayList<String>();
 		final StringBuffer constructPattern = new StringBuffer("CONSTRUCT { ");
 		final StringBuffer wherePattern = new StringBuffer("WHERE { <" + key + "> a ?o . ");
 		constructPattern.append(" <" + key + "> a ?o . ");
-		
-		for (EStructuralFeature aFeature: eClass.getEAllStructuralFeatures()) {
+
+		for (EStructuralFeature aFeature: feats) {
 			if (EntityUtil.getId(eClass) != null && !EntityUtil.getId(eClass).equals(aFeature)) {
 				URI rdfType = EntityUtil.getRdfType(aFeature);
 				constructPattern.append(" <" + key + "> <" + rdfType + "> ?" + aFeature.getName() + " . ");
-				wherePattern.append(" OPTIONAL { <" + key + "> <" + rdfType + "> ?" +
-						aFeature.getName() 
-						+ " } ");
+				wherePattern.append(" OPTIONAL { <" + key + "> <" + rdfType + "> ?" + aFeature.getName() + " } ");
 			}
 		}
 		constructPattern.append(" } ");
-		wherePattern.append(" } LIMIT 800");
+		wherePattern.append(" } ");
 		constructPattern.append(wherePattern);
-		
-		return constructPattern.toString();
+
+		queries.add(constructPattern.toString());
+
+		return queries;
 	}
-	
+
 	public static String constructSubject(URI key, Object object, Integer limit) {
-		return "CONSTRUCT { <" + key + "> ?p ?o } WHERE { <" + key + "> ?p ?o }" + 
-			((limit != null) ? " LIMIT " + limit : "");
+		return "CONSTRUCT { <" + key + "> ?p ?o } WHERE { <" + key + "> ?p ?o }" + ((limit != null) ? " LIMIT " + limit : "");
 	}
 }
